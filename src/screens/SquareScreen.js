41f19fd05b5af65be509b1b8fa474ed0bddf4c89
Maksym Shevchenko <m.shevchenko@astoundcommerce.com>
2019-10-04 18:32:16 +0300

    Change color with useReducer

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,31 +1,55 @@
-import React, {useState} from 'react';
+import React, {useReducer} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import NumericInput from 'react-native-numeric-input';
 import ColorAdjuster from '../components/ColorAdjuster';
 
-const SquareScreen = (props) => {
-    const [red, setRed] = useState(0);
-    const [green, setGreen] = useState(0);
-    const [blue, setBlue] = useState(0);
-    const [rateOfChange, setRateOfChange] = useState(10);
+const CHANGE_RED = 'CHANGE_RED';
+const CHANGE_GREEN = 'CHANGE_GREEN';
+const CHANGE_BLUE = 'CHANGE_BLUE';
+const CHANGE_RATE = 'CHANGE_RATE';
 
-    const checkRange = (colorValue, setColor) => {
-        if (colorValue > 256) return;
-        if (colorValue < 0) return;
-        return setColor(colorValue);
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case CHANGE_RED:
+            return {...state, red: action.payload};
+        case CHANGE_GREEN:
+            return {...state, green: action.payload};
+        case CHANGE_BLUE:
+            return {...state, blue: action.payload};
+        case CHANGE_RATE:
+            return {...state, rateOfChange: action.payload};
+        default:
+            return state
+    }
+};
+
+const SquareScreen = (props) => {
+
+    const [state, dispatch] = useReducer(reducer, {
+        red: 0,
+        green: 0,
+        blue: 0,
+        rateOfChange: 10
+    });
+
+    const {red, green, blue, rateOfChange} = state;
+
+    const changeStateValue = (type, payload) => {
+        if (payload > 256) payload = 256;
+        if (payload < 0) payload = 0;
+        dispatch({type, payload});
     };
 
-    const increaseRed = () => checkRange(red + rateOfChange, setRed);
-    const increaseGreen = () => checkRange(green + rateOfChange, setGreen);
-    const increaseBlue = () => checkRange(blue + rateOfChange, setBlue);
-
-    const decreaseRed = () => checkRange(red - rateOfChange, setRed);
-    const decreaseGreen = () => checkRange(green - rateOfChange, setGreen);
-    const decreaseBlue = () => checkRange(blue - rateOfChange, setBlue);
+    const increaseRed = () => changeStateValue(CHANGE_RED, red + rateOfChange);
+    const decreaseRed = () => changeStateValue(CHANGE_RED, red - rateOfChange);
+    const increaseGreen = () => changeStateValue(CHANGE_GREEN, green + rateOfChange);
+    const decreaseGreen = () => changeStateValue(CHANGE_GREEN, green - rateOfChange);
+    const increaseBlue = () => changeStateValue(CHANGE_BLUE, blue + rateOfChange);
+    const decreaseBlue = () => changeStateValue(CHANGE_BLUE, blue - rateOfChange);
+    const handleRateOfChange = value => dispatch({type: CHANGE_RATE, payload: value});
 
     const generateColorString = () => `Red: ${red}\nGreen: ${green}\nBlue: ${blue}`;
-
-    const handleRateOfChange = value => setRateOfChange(value);
 
     return (
         <View>
